0fb1e08fe24a331c3c4c503a704295e86caefd08
marerwis <marerwis@gmail.com>
2026-07-16 02:48:49 +0200

    Fix missing Supabase imports in Admin Panel

--- shamel_admin_panel/lib/features/providers/screens/providers_management_screen.dart
+++ shamel_admin_panel/lib/features/providers/screens/providers_management_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:intl/intl.dart';
 import '../../../../core/theme/app_colors.dart';
 import '../../members/providers/members_provider.dart';
