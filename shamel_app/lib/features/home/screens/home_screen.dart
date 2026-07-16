1706273552839ecec27dd0a349578a9573e50461
marerwis <marerwis@gmail.com>
2026-07-16 13:43:13 +0200

    Fix syntax error in home_screen

--- shamel_app/lib/features/home/screens/home_screen.dart
+++ shamel_app/lib/features/home/screens/home_screen.dart
@@ -209,7 +209,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                               ],
                             ),
                           ),
-                        );
+                        ));
                       },
                     );
                   }).toList(),
