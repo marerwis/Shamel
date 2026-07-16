28089e75dddb446c6749bfc7c7682c1193ddde65
marerwis <marerwis@gmail.com>
2026-07-16 14:01:55 +0200

    Fix AspectRatio syntax error

--- shamel_app/lib/features/home/screens/home_screen.dart
+++ shamel_app/lib/features/home/screens/home_screen.dart
@@ -211,7 +211,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                               ],
                             ),
                           ),
-                        ));
+                        )));
                       },
                     );
                   }).toList(),
