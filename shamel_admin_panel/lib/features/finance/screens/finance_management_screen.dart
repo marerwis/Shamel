fbdd86aebd68522fb9ee02d7beb5f91146bcf2b6
marerwis <marerwis@gmail.com>
2026-07-16 05:09:45 +0200

    Fix missing Supabase import in Finance screen

--- shamel_admin_panel/lib/features/finance/screens/finance_management_screen.dart
+++ shamel_admin_panel/lib/features/finance/screens/finance_management_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../../../core/theme/app_colors.dart';
 import '../providers/finance_provider.dart';
 
